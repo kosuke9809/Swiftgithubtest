c30caff6cf7c50d466b7959a1288387be8ad501d
kosuke9809 <uht0912k@outlook.com>
2020-08-08 22:44:16 +0900

    Initial Commit

---
+++ Swiftgithubtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftgithubtest
+//
+//  Created by 上畠　康輔 on 2020/08/08.
+//  Copyright © 2020 Kosuke Uehata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
