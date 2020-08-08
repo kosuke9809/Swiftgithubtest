90edca0e81319327892ae70908abb557c7eadc57
kosuke9809 <uht0912k@outlook.com>
2020-08-08 23:26:23 +0900

    secondcommit

--- Swiftgithubtest/ViewController.swift
+++ Swiftgithubtest/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var githubtest: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func testbutton(_ sender: Any) {
+    }
+    
 }
 
